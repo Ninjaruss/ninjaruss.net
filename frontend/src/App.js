3bd1dd72c263e5d33de423c878b426c580747bfd
Ninjaruss <russ081999@gmail.com>
2022-12-03 00:03:36 -0800

    Base prototype for express/react application.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component} from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import LandingPage from "./components/Landing/LandingPage";
+import BlogPage from "./components/Blog/BlogPage";
+import UndefinedPage from "./components/UndefinedPage"; 
+
+class App extends Component {
+  render() { 
+    return (
+      <div className="App">
+        <React.Fragment>
+          <Router>
+            <Routes>
+              <Route exact path="/" element={<LandingPage />} />*
+              <Route path="/blog" element={<BlogPage />} />*
+              <Route element={<UndefinedPage />} />
+            </Routes>
+          </Router>
+        </React.Fragment>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ frontend/src/components/Landing/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Button} from "react-bootstrap";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Landing page</h1>
+    </div>
+  )
+}
+
+export default LandingPage
