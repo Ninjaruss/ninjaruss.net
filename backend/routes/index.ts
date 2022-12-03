a6c1ae7df3104b4eac0b756d1720a0ae28956f1e
Ninjaruss <russ081999@gmail.com>
2022-12-02 23:37:14 -0800

    Reset application to not use TypeScripte.

--- backend/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import express, {Request, Response, Router} from "express";
-let router = Router();
-
-/* GET home page. */
-router.get('/', function(req: Request, res: Response, next: any) {
-  return res.send('index')
-});
-
-export {router};
--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,17 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
+import express, {Request, Response} from "express";
+import path from "path";
+const app = express();
 const port = 3001;
+
 // middleware
-app.use(express_1.default.json());
-const index_1 = require("./routes/index");
+app.use(express.json());
+
+import {router as indexRouter} from './routes/index';
 // var usersRouter = require('./routes/users.ts');
-app.use('/api', index_1.router);
+
+app.use('/api', indexRouter);
 // app.use('/api/users', usersRouter);
-/*
+
 app.use(
   express.static(path.join(__dirname, "../frontend/build"))
 )
@@ -29,23 +19,21 @@ app.use(
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
-*/
+
+
 app.all("/app/all"), (req, res) => {
-    return res.sendStatus(200);
-};
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', (req, res) => {
-    return res.send('hello world');
-});
-function throwsError() {
-    return __awaiter(this, void 0, void 0, function* () {
-        throw new Error("Rip...");
-    });
+  return res.sendStatus(200);
 }
-app.get("/error"), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    yield throwsError();
-    res.send('ok');
-});
+
+async function throwsError(){
+  throw new Error("Rip...");
+}
+
+app.get("/error"), async (req, res) => {
+  await throwsError();
+  res.send('ok');
+}
+
 app.listen(port, () => {
-    console.log(`Application listening on port: ${port}`);
-});
+  console.log(`Application listening on port: ${port}`);
+})
