8833caae277e5ef40e9654059723e4fe41fd9ff7
Ninjaruss <russ081999@gmail.com>
2026-05-19 12:50:42 -0700

    feat(shelf): update favorites redirects and add /media → /shelf config redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://ninjaruss.net',
   adapter: vercel(),
   integrations: [mdx(), sitemap()],
+  redirects: {
+    '/media': '/shelf',
+    '/media/[...slug]': '/shelf/[...slug]',
+  },
 });
